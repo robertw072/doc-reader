98d6202f7d679d12d5bc99137e17e913e957199b
Robert P. Williamson <williamson_rp@cobra.cs.mercer.edu>
2019-09-01 15:58:35 -0400

    Sentence counter working properly

--- doc-reader.cpp
+++ doc-reader.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include <string>
 
 using namespace std;
+
+bool isNumber (const char* input, int base);
 
 int main()
 {
@@ -14,30 +17,67 @@ int main()
 		cout << "The file opened correctly." << endl;
 	}
 	
-	else cout << "Unable to open file." << endl;
+	else cout << "Unable to open the file. " << endl;
 
-	int word = 1, sentence = 0;							// first word will not be counted so its initial val is 1
-	char ch;
+	/*int sentence = 0;
+        char ch;
 
-	while (myFile)
+        while (myFile)                                                                  // this loop gets the sentence count
+        {
+                myFile.get(ch);
+
+                if (ch == '.' || ch == ':' || ch == ';' || ch
+                == '?' || ch == '!')
+                {
+                        sentence++;
+                }
+        }
+
+        cout << "Sentence Count: " << sentence << endl;*/
+
+	string contents((istreambuf_iterator<char>(myFile)),				// puts the file into a string
+	 istreambuf_iterator<char>());
+
+	char *contentsCopy = new char [contents.length() + 1];				// instantiates a character array which holds a copy of contents
+	strcpy (contentsCopy, contents.c_str());
+
+	char *token = strtok(contentsCopy, " ");					// tokenizes contents and puts in a token array
+	
+	int word = 0;
+	int sentence = 0;
+
+	while (token != NULL)								// this loops counts the number of words
 	{
-		myFile.get(ch);
-	
-		if (ch == ' ' || ch == '\n')
-			word++;
+		//cout << token << endl;
+		word++;
 
-		if (ch == '.' || ch == ':' || ch == ';' || ch == 
-		'?' || ch == '!')
+		for (int i = 0; i < strlen(token); i++)					// this loop counts the number of sentences
 		{
-			sentence++;
-		}
+			char ch = token[i];		
 	
+			if (ch == '.' || ch == ':'
+			|| ch == ';' || ch == '?'
+			|| ch == '!')
+			{
+				sentence++;	
+			} 
+		}
+
+		token = strtok(NULL, " ");
 	}
 
-	cout << "Word Count: " << word << endl;
-	cout << "Sentence Count: " << sentence << endl;
+	cout << "Word count: " << word << endl;	
+	cout << "Sentence count: " << sentence << endl;
 
 	myFile.close();	
 
 	return 0;
 }
+
+bool isNumber (const char* input, int base)
+{
+	string numbase = "0123456789ABCDEF";
+	string in = input;
+
+	return (in.find_first_not_of(numbase.substr(0, base)) == string::npos);
+} 
